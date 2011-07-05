82fdefe470a411c4f39daeb29f70677a7570a529
Eric GELOEN <geloen.eric@gmail.com>
2011-07-05 09:05:59 +0200

    Starting JQueryBundle

---
+++ IvoryJQueryBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ivory\JQueryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * JQuery bundle
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class IvoryJQueryBundle extends Bundle
+{
+}
